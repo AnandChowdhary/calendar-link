49dfe03da5d499f29a1aab8a7aaabe136a4f91ef
Kam <kam@snaptravel.com>
2019-08-22 18:38:42 -0400

    fixed test timezone issue

--- index.test.ts
+++ index.test.ts
@@ -1,102 +1,149 @@
+import dayjs from "dayjs";
+
 import { google, yahoo, outlook } from "./index";
+import { TimeFormats } from "./utils";
+import { CalendarEvent } from "./interfaces";
 
-test("generate a google link", () => {
-  const link = google({
-    title: "Birthday party",
-    start: "2019-12-29",
-    duration: [2, "hour"]
-  });
-  expect(link).toBe(
-    "https://calendar.google.com/calendar/render?action=TEMPLATE&text=Birthday%20party&details=&location=&trp=&dates=20191229T000000Z%2F20191229T020000Z"
-  );
-});
+describe('Calendar Links', () => {
+  describe('Google', () => {
+    test("generate a google link", () => {
+      const event: CalendarEvent = {
+        title: "Birthday party",
+        start: "2019-12-29",
+        duration: [2, "hour"]
+      }
+      const link = google(event);
+      const sTime = dayjs(event.start).utc().format(TimeFormats.dateTimeUTC)
+      const eTime = dayjs(event.start).add(2, 'hour').utc().format(TimeFormats.dateTimeUTC)
+      const expectedDates = encodeURIComponent(`${sTime}/${eTime}`)
+      expect(link).toBe(
+        `https://calendar.google.com/calendar/render?action=TEMPLATE&text=Birthday%20party&details=&location=&trp=&dates=${expectedDates}`
+      );
+    });
 
-test("generate a google link with time & timezone", () => {
-  const link = google({
-    title: "Birthday party",
-    start: "2019-12-29T12:00:00.000+01:00",
-    duration: [2, "hour"]
-  });
-  expect(link).toBe(
-    "https://calendar.google.com/calendar/render?action=TEMPLATE&text=Birthday%20party&details=&location=&trp=&dates=20191229T110000Z%2F20191229T130000Z"
-  );
-});
+    test("generate a google link with time & timezone", () => {
+      const event: CalendarEvent = {
+        title: "Birthday party",
+        start: "2019-12-29T12:00:00.000+01:00",
+        duration: [2, "hour"]
+      }
+      const link = google(event);
+      const sTime = dayjs(event.start).utc().format(TimeFormats.dateTimeUTC)
+      const eTime = dayjs(event.start).add(2, 'hour').utc().format(TimeFormats.dateTimeUTC)
+      const expectedDates = encodeURIComponent(`${sTime}/${eTime}`)
+      expect(link).toBe(
+        `https://calendar.google.com/calendar/render?action=TEMPLATE&text=Birthday%20party&details=&location=&trp=&dates=${expectedDates}`
+      );
+    });
 
-test("generate an all day google link", () => {
-  const link = google({
-    title: "Birthday party",
-    start: "2019-12-29",
-    allDay: true
-  });
-  expect(link).toBe(
-    "https://calendar.google.com/calendar/render?action=TEMPLATE&text=Birthday%20party&details=&location=&trp=&dates=20191229%2F20191230"
-  );
-});
+    test("generate an all day google link", () => {
+      const event: CalendarEvent = {
+        title: "Birthday party",
+        start: "2019-12-29",
+        allDay: true
+      }
+      const link = google(event);
+      const sTime = dayjs(event.start).utc().format(TimeFormats.allDay)
+      const eTime = dayjs(event.start).add(1, 'day').utc().format(TimeFormats.allDay)
+      const expectedDates = encodeURIComponent(`${sTime}/${eTime}`)
+      expect(link).toBe(
+        `https://calendar.google.com/calendar/render?action=TEMPLATE&text=Birthday%20party&details=&location=&trp=&dates=${expectedDates}`
+      );
+    });
 
-test("generate a multi day google link", () => {
-  const link = google({
-    title: "Birthday party",
-    start: "2019-12-29",
-    end: "2020-01-12",
-    allDay: true
-  });
-  expect(link).toBe(
-    "https://calendar.google.com/calendar/render?action=TEMPLATE&text=Birthday%20party&details=&location=&trp=&dates=20191229%2F20200112"
-  );
-});
+    test("generate a multi day google link", () => {
+      const event: CalendarEvent = {
+        title: "Birthday party",
+        start: "2019-12-29",
+        end: "2020-01-12",
+        allDay: true
+      }
+      const link = google(event);
+      const sTime = dayjs(event.start).utc().format(TimeFormats.allDay)
+      const eTime = dayjs(event.end).utc().format(TimeFormats.allDay)
+      const expectedDates = encodeURIComponent(`${sTime}/${eTime}`)
+      expect(link).toBe(
+        `https://calendar.google.com/calendar/render?action=TEMPLATE&text=Birthday%20party&details=&location=&trp=&dates=${expectedDates}`
+      );
+    });
 
-test("generate a google link with guests", () => {
-  const link = google({
-    title: "Birthday party",
-    start: "2019-12-29",
-    duration: [2, "hour"],
-    guests: ["hello@example.com", "another@example.com"]
+    test("generate a google link with guests", () => {
+      const event: CalendarEvent = {
+        title: "Birthday party",
+        start: "2019-12-29",
+        duration: [2, "hour"],
+        guests: ["hello@example.com", "another@example.com"]
+      }
+      const link = google(event);
+      const sTime = dayjs(event.start).utc().format(TimeFormats.dateTimeUTC)
+      const eTime = dayjs(event.start).add(2, 'hour').utc().format(TimeFormats.dateTimeUTC)
+      const expectedDates = encodeURIComponent(`${sTime}/${eTime}`)
+      const expectedGuests = encodeURIComponent(event.guests ? event.guests.join() : '')
+      expect(link).toBe(
+        `https://calendar.google.com/calendar/render?action=TEMPLATE&text=Birthday%20party&details=&location=&trp=&dates=${expectedDates}&add=${expectedGuests}`
+      );
+    });
   });
-  expect(link).toBe(
-    "https://calendar.google.com/calendar/render?action=TEMPLATE&text=Birthday%20party&details=&location=&trp=&dates=20191229T000000Z%2F20191229T020000Z&add=hello%40example.com%2Canother%40example.com"
-  );
-});
+  describe('Yahoo', () => {
+    test("generate a yahoo link", () => {
+      const event: CalendarEvent = {
+        title: "Birthday party",
+        start: "2019-12-29",
+        duration: [2, "hour"]
+      }
+      const link = yahoo(event);
+      const sTime: String = dayjs(event.start).utc().format(TimeFormats.dateTimeUTC)
+      const eTime: String = dayjs(event.start).add(2, 'hour').utc().format(TimeFormats.dateTimeUTC)
 
-test("generate a yahoo link", () => {
-  const link = yahoo({
-    title: "Birthday party",
-    start: "2019-12-29",
-    duration: [2, "hour"]
-  });
-  expect(link).toBe(
-    "https://calendar.yahoo.com/?v=60&title=Birthday%20party&st=20191229T000000Z&et=20191229T020000Z&desc=&in_loc="
-  );
-});
+      expect(link).toBe(
+        `https://calendar.yahoo.com/?v=60&title=Birthday%20party&st=${sTime}&et=${eTime}&desc=&in_loc=`
+      );
+    });
 
-test("generate an all day yahoo link", () => {
-  const link = yahoo({
-    title: "Birthday party",
-    start: "2019-12-29",
-    allDay: true
-  });
-  expect(link).toBe(
-    "https://calendar.yahoo.com/?v=60&title=Birthday%20party&st=20191229&et=20191230&desc=&in_loc="
-  );
-});
+    test("generate an all day yahoo link", () => {
+      const event: CalendarEvent = {
+        title: "Birthday party",
+        start: "2019-12-29",
+        allDay: true
+      }
+      const link = yahoo(event);
+      const sTime: String = dayjs(event.start).utc().format(TimeFormats.allDay)
+      const eTime: String = dayjs(event.start).add(1, 'day').utc().format(TimeFormats.allDay)
 
-test("generate a outlook link", () => {
-  const link = outlook({
-    title: "Birthday party",
-    start: "2019-12-29",
-    duration: [2, "hour"]
+      expect(link).toBe(
+        `https://calendar.yahoo.com/?v=60&title=Birthday%20party&st=${sTime}&et=${eTime}&desc=&in_loc=`
+      );
+    });
   });
-  expect(link).toBe(
-    "https://outlook.live.com/owa/?path=%2Fcalendar%2Faction%2Fcompose&rru=addevent&startdt=20191229T000000&enddt=20191229T020000&subject=Birthday%20party&body=&location="
-  );
-});
+  describe('Outlook', () => {
+    test("generate a outlook link", () => {
+      const event: CalendarEvent = {
+        title: "Birthday party",
+        start: "2019-12-29",
+        duration: [2, "hour"]
+      }
+      const sTime: String = dayjs(event.start).utc().format(TimeFormats.dateTime)
+      const eTime: String = dayjs(event.start).add(2, 'hour').utc().format(TimeFormats.dateTime)
+      const link = outlook(event);
 
-test("generate an all day outlook link", () => {
-  const link = outlook({
-    title: "Birthday party",
-    start: "2019-12-29",
-    allDay: true
+      expect(link).toBe(
+        `https://outlook.live.com/owa/?path=%2Fcalendar%2Faction%2Fcompose&rru=addevent&startdt=${sTime}&enddt=${eTime}&subject=Birthday%20party&body=&location=`
+      );
+    });
+
+    test("generate an all day outlook link", () => {
+      const event: CalendarEvent = {
+        title: "Birthday party",
+        start: "2019-12-29",
+        allDay: true
+      }
+      const link = outlook(event);
+      const sTime: String = dayjs(event.start).utc().format(TimeFormats.allDay)
+      const eTime: String = dayjs(event.start).add(1, 'day').utc().format(TimeFormats.allDay)
+
+      expect(link).toBe(
+        `https://outlook.live.com/owa/?path=%2Fcalendar%2Faction%2Fcompose&rru=addevent&startdt=${sTime}&enddt=${eTime}&subject=Birthday%20party&body=&location=`
+      );
+    });
   });
-  expect(link).toBe(
-    "https://outlook.live.com/owa/?path=%2Fcalendar%2Faction%2Fcompose&rru=addevent&startdt=20191229&enddt=20191230&subject=Birthday%20party&body=&location="
-  );
-});
+})
