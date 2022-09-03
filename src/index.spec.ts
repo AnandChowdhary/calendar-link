1ad110fafe1311d30d8d3bb6eb8088c7ca837922
Joed Parin <parinjoed@gmail.com>
2022-09-03 14:47:08 +0800

    test: add unit test for the outlook link with a valid http url inside the description, make that url a hyperlink

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,7 @@
 import dayjs from "dayjs";
 
 import { google, yahoo, outlook, office365, ics } from "./index";
-import { TimeFormats } from "./utils";
+import { TimeFormats, wrapHttpUrlInAnchorTag } from "./utils";
 import { CalendarEvent } from "./interfaces";
 
 describe("Calendar Links", () => {
@@ -83,5 +83,36 @@ describe("Calendar Links", () => {
         `https://calendar.google.com/calendar/render?action=TEMPLATE&add=${expectedGuests}&dates=${expectedDates}&text=Birthday%20party`
       );
     });
+  });
+
+  describe("Outlook", () => {
+    test("generate a outlook link without valid http url in the description", () => {
+      const event: CalendarEvent = {
+        title: "Birthday party",
+        start: "2022-09-03",
+        description: "I hope you come to my birthday",
+        duration: [2, "hour"],
+      };
+      const link = outlook(event);
+      expect(link).toBe(
+        `https://outlook.live.com/calendar/0/deeplink/compose?allday=false&body=I%20hope%20you%20come%20to%20my%20birthday&enddt=2022-09-02T18%3A00%3A00%2B00%3A00&path=%2Fcalendar%2Faction%2Fcompose&rru=addevent&startdt=2022-09-02T16%3A00%3A00%2B00%3A00&subject=Birthday%20party`
+      );
+    });
+
+    test("generate a outlook link with valid http url in the description", () => {
+      const event: CalendarEvent = {
+        title: "Birthday party",
+        start: "2022-09-03T12:00:00.000+00:00",
+        description: "I hope you come to my birthday here is the site of the event place https://www.npmjs.com/package/calendar-link  Thank you.",
+        duration: [1, "hour"],
+      };
+      const link = outlook(event);
+      const expectedBody = encodeURIComponent(wrapHttpUrlInAnchorTag(event.description) ?? '').replace(/[!'()*]/g, escape);
+
+      expect(link).toBe(
+        `https://outlook.live.com/calendar/0/deeplink/compose?allday=false&body=${expectedBody}&enddt=2022-09-03T13%3A00%3A00%2B00%3A00&path=%2Fcalendar%2Faction%2Fcompose&rru=addevent&startdt=2022-09-03T12%3A00%3A00%2B00%3A00&subject=Birthday%20party`
+      );
+    });
+  
   });
 });
