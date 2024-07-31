6da873c9f5d9aa4a26f39acdab6d905e7dd9230f
Braeden Kilburn <61486941+BraedenKilburn@users.noreply.github.com>
2024-07-31 01:46:43 -0700

    :bug: Fix Snapshot Tests for ICS Links (#600)
    * test: update ics link snapshots and wrap tests in appropriate describe block
    
    * fix: test-watch works and runs immediately
    
    * revert: dont watchAll, just use watch
    
    ---------
    
    Co-authored-by: Braeden Kilburn <braeden.kilburn@vivint.com>

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,5 @@ module.exports = {
   preset: "ts-jest",
   globalSetup: "./jest-global-setup.js",
   testEnvironment: "node",
+  watchPathIgnorePatterns: ['node_modules'],
 };
--- src/index.spec.ts
+++ src/index.spec.ts
@@ -20,67 +20,79 @@ for (const service of [
   outlookMobile,
   yahoo,
 ]) {
-  describe(`${service.name} service`, () => {});
-  test(`generate a ${service.name} link`, () => {
-    const event: CalendarEvent = {
-      title: "Birthday party",
-      start: "2019-12-29",
-      duration: [2, "hour"],
-    };
-    const link = service(event);
-    expect(link).toMatchSnapshot();
+  beforeAll(() => {
+    jest.useFakeTimers("modern");
+    jest.setSystemTime(new Date("2019-12-28T12:00:00.000Z"));
+
+    jest.spyOn(global.Math, "random").mockReturnValue(0.12345);
   });
 
-  test(`generate a ${service.name} link with time & timezone`, () => {
-    const event: CalendarEvent = {
-      title: "Birthday party",
-      start: "2019-12-29T12:00:00.000+01:00",
-      duration: [2, "hour"],
-    };
-    const link = service(event);
-    expect(link).toMatchSnapshot();
+  afterAll(() => {
+    jest.useRealTimers();
   });
 
-  test(`generate an all day ${service.name} link`, () => {
-    const event: CalendarEvent = {
-      title: "Birthday party",
-      start: "2019-12-29",
-      allDay: true,
-    };
-    const link = service(event);
-    expect(link).toMatchSnapshot();
-  });
+  describe(`${service.name} service`, () => {
+    test(`generate a ${service.name} link`, () => {
+      const event: CalendarEvent = {
+        title: "Birthday party",
+        start: "2019-12-29",
+        duration: [2, "hour"],
+      };
+      const link = service(event);
+      expect(link).toMatchSnapshot();
+    });
 
-  test(`generate a multi day ${service.name} link`, () => {
-    const event: CalendarEvent = {
-      title: "Birthday party",
-      start: "2019-12-29",
-      end: "2020-01-12",
-      allDay: true,
-    };
-    const link = service(event);
-    expect(link).toMatchSnapshot();
-  });
+    test(`generate a ${service.name} link with time & timezone`, () => {
+      const event: CalendarEvent = {
+        title: "Birthday party",
+        start: "2019-12-29T12:00:00.000+01:00",
+        duration: [2, "hour"],
+      };
+      const link = service(event);
+      expect(link).toMatchSnapshot();
+    });
 
-  test(`generate a recurring ${service.name} link`, () => {
-    const event: CalendarEvent = {
-      title: "Birthday party",
-      start: "2019-12-29",
-      duration: [2, "hour"],
-      rRule: "FREQ=YEARLY;INTERVAL=1",
-    };
-    const link = service(event);
-    expect(link).toMatchSnapshot();
-  });
+    test(`generate an all day ${service.name} link`, () => {
+      const event: CalendarEvent = {
+        title: "Birthday party",
+        start: "2019-12-29",
+        allDay: true,
+      };
+      const link = service(event);
+      expect(link).toMatchSnapshot();
+    });
 
-  test(`generate a ${service.name} link with guests`, () => {
-    const event: CalendarEvent = {
-      title: "Birthday party",
-      start: "2019-12-29",
-      duration: [2, "hour"],
-      guests: ["hello@example.com", "another@example.com"],
-    };
-    const link = service(event);
-    expect(link).toMatchSnapshot();
+    test(`generate a multi day ${service.name} link`, () => {
+      const event: CalendarEvent = {
+        title: "Birthday party",
+        start: "2019-12-29",
+        end: "2020-01-12",
+        allDay: true,
+      };
+      const link = service(event);
+      expect(link).toMatchSnapshot();
+    });
+
+    test(`generate a recurring ${service.name} link`, () => {
+      const event: CalendarEvent = {
+        title: "Birthday party",
+        start: "2019-12-29",
+        duration: [2, "hour"],
+        rRule: "FREQ=YEARLY;INTERVAL=1",
+      };
+      const link = service(event);
+      expect(link).toMatchSnapshot();
+    });
+
+    test(`generate a ${service.name} link with guests`, () => {
+      const event: CalendarEvent = {
+        title: "Birthday party",
+        start: "2019-12-29",
+        duration: [2, "hour"],
+        guests: ["hello@example.com", "another@example.com"],
+      };
+      const link = service(event);
+      expect(link).toMatchSnapshot();
+    });
   });
 }
