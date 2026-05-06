2ea6b1629f186bde9b13b39858b2b294279a5b1c
Anand Chowdhary <github@anandchowdhary.com>
2026-05-06 12:04:42 +0200

    :construction_worker: Migrate releases to npm trusted publishing
    Replace the old shared @koj/config release setup with an in-repo semantic-release config and migrate npm publishing to GitHub Actions trusted publishing/OIDC.
    
    - upgrade semantic-release and plugins for trusted publishing support
    - remove NPM_TOKEN from the release workflow
    - add id-token: write permissions and modern Node/npm usage
    - run tests explicitly before build/release
    
    Fixes #695

--- release.config.js
+++ release.config.js
@@ -1,1 +1,65 @@
-module.exports = require("@koj/config").releaseMaster;
+module.exports = {
+  branches: ["master"],
+  plugins: [
+    [
+      "semantic-release-gitmoji",
+      {
+        releaseRules: {
+          major: { include: [":boom:"] },
+          minor: { include: [":sparkles:"] },
+          patch: {
+            include: [
+              ":bug:",
+              ":ambulance:",
+              ":lock:",
+              ":recycle:",
+              ":lipstick:",
+              ":alien:",
+              ":package:",
+            ],
+          },
+        },
+        releaseNotes: {
+          template:
+            '## v{{nextRelease.version}} ({{datetime "UTC:yyyy-mm-dd"}})\n\n' +
+            '[📝 Release notes](https://github.com/{{owner}}/{{repo}}/releases/tag/v{{nextRelease.version}})' +
+            '{{#if compareUrl}} · [💻 Compare]({{compareUrl}}){{/if}} · ' +
+            '[🔖 Tag](https://github.com/{{owner}}/{{repo}}/tree/v{{nextRelease.version}}) · ' +
+            '🗄️ Archive ([zip](https://github.com/{{owner}}/{{repo}}/archive/v{{nextRelease.version}}.zip) · ' +
+            '[tar.gz](https://github.com/{{owner}}/{{repo}}/archive/v{{nextRelease.version}}.tar.gz))\n\n' +
+            '{{#with commits}}\n' +
+            '{{#if sparkles}}\n### ✨ New features\n\n{{#each sparkles}}- {{> commitTemplate}}{{/each}}\n{{/if}}\n' +
+            '{{#if recycle}}\n### ♻️ Updates\n\n{{#each recycle}}- {{> commitTemplate}}{{/each}}\n{{/if}}\n' +
+            '{{#if lipstick}}\n### 💄 Interface changes\n\n{{#each lipstick}}- {{> commitTemplate}}{{/each}}\n{{/if}}\n' +
+            '{{#if bug}}\n### 🐛 Bug fixes\n\n{{#each bug}}- {{> commitTemplate}}{{/each}}\n{{/if}}\n' +
+            '{{#if ambulance}}\n### 🚑 Critical hotfixes\n\n{{#each ambulance}}- {{> commitTemplate}}{{/each}}\n{{/if}}\n' +
+            '{{#if lock}}\n### 🔒 Security issues\n\n{{#each lock}}- {{> commitTemplate}}{{/each}}\n{{/if}}\n' +
+            '{{#if arrow_up}}\n### ⬆️ Dependency updates\n\n{{#each arrow_up}}- {{> commitTemplate}}{{/each}}\n{{/if}}\n' +
+            '{{#if boom}}\n### 💥 Breaking changes\n\n{{#each boom}}- {{> commitTemplate}}{{/each}}\n{{/if}}\n' +
+            '{{/with}}',
+          partials: {
+            commitTemplate:
+              '[`{{commit.short}}`](https://github.com/{{owner}}/{{repo}}/commit/{{commit.short}}) {{subject}}\n' +
+              '{{#if issues}}(Issues:{{#each issues}} [`{{text}}`]({{link}}){{/each}}){{/if}}' +
+              '{{#if wip}}{{#each wip}}\n- [`{{commit.short}}`](https://github.com/{{owner}}/{{repo}}/commit/{{commit.short}}) {{subject}}{{/each}}\n{{/if}}',
+          },
+          issueResolution: {
+            template: "{baseUrl}/{owner}/{repo}/issues/{ref}",
+            baseUrl: "https://github.com",
+            source: "github.com",
+          },
+        },
+      },
+    ],
+    "@semantic-release/github",
+    ["@semantic-release/changelog", { changelogFile: "CHANGELOG.md" }],
+    "@semantic-release/npm",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md", "package.json", "package-lock.json"],
+        message: ":bookmark: Release v${nextRelease.version} [skip ci]",
+      },
+    ],
+  ],
+};
